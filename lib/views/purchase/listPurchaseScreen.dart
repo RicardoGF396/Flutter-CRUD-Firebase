61455c7310cc6b6b073b856363d22303108c12a6
Efrain Camargo <Josephxrex.jc72705@udelasalle.edu.mx>
2023-05-13 23:42:45 -0600

    add-AddFun

--- lib/views/purchase/listPurchaseScreen.dart
+++ lib/views/purchase/listPurchaseScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase/views/services/purchase-service/firebase_service.dart';
+import 'package:flutter_firebase/components/headerList.dart';
 
 class ListPurchaseScreen extends StatefulWidget {
   @override
@@ -9,31 +10,87 @@ class ListPurchaseScreen extends StatefulWidget {
 class _ListPurchaseScreenState extends State<ListPurchaseScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Purchase")),
-      body: FutureBuilder(
-        future: getPurchase(),
-        builder: ((context, snapshot) {
-          if (snapshot.hasData) {
-            return ListView.builder(
-              itemCount: snapshot.data?.length,
-              itemBuilder: (context, index) {
-                return Text(snapshot.data?[index]['name']);
+   return Scaffold(
+      backgroundColor: Color.fromRGBO(25, 23, 32, 1),
+      extendBody: true,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12),
+        child: Column(
+          children: [
+            HeaderList(
+              title: "Users",
+              onBack: () {
+                Navigator.pop(context);
               },
-            );
-          } else {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        }),
+            ),
+            FutureBuilder(
+              future: getPurchase(),
+              builder: ((context, snapshot) {
+                if (snapshot.hasData) {
+                  return Expanded(
+                    child: ListView.builder(
+                      itemCount: snapshot.data?.length,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          margin: EdgeInsets.only(bottom: 12),
+                          child: ListTile(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8.0),
+                              side:
+                                  BorderSide(color: Color(0xFF3D3C46), width: 2),
+                            ),
+                            leading: Icon(Icons.account_circle_sharp,
+                                color: Colors.white, size: 40),
+                            trailing: Icon(
+                              Icons.edit,
+                              color: Color.fromARGB(150, 230, 230, 230),
+                            ),
+                            title: Padding(
+                              padding: const EdgeInsets.only(top: 12),
+                              child: Text(
+                                snapshot.data?[index]['name'],
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                            subtitle: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Pieces: " + snapshot.data?[index]['pieces'],
+                                  style: TextStyle(
+                                      color: Color.fromARGB(150, 230, 230, 230)),
+                                ),
+                                Text(
+                                  "IDA: " + snapshot.data?[index]['ida'],
+                                  style: TextStyle(
+                                      color: Color.fromARGB(150, 230, 230, 230)),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  );
+                } else {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+              }),
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          await Navigator.pushNamed(context,'/addPurchase');
-          setState((){});
+        backgroundColor: Colors.white,
+        onPressed: () {
+          Navigator.pushNamed(context, '/addPurchase');
         },
-        child: const Icon(Icons.add),
+        child: const Icon(
+          Icons.add,
+          color: Colors.black,
+        ),
       ),
     );
   }
