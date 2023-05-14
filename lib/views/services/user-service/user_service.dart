7eb0fac6a429c3e4a856da0d96459575390ea838
Ricardo <ricardogf396@gmail.com>
2023-05-13 22:57:19 -0600

    Lista de user terminada

---
+++ lib/views/services/user-service/user_service.dart
@@ -0,0 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+//Aqui va toda la funcionalidad de firestore
+FirebaseFirestore db = FirebaseFirestore.instance;
+
+//============== Purchase CRUD =================
+
+//==>Obtener todos las compras
+Future<List> getUsers() async {
+  List users = [];
+  //Hacemos refencia a la colección de FireBase
+  CollectionReference collectionReferencePurchase = db.collection('user');
+  //Traemos por medio del metodo get todo lo que tiene la colección
+  QuerySnapshot queryPurchase = await collectionReferencePurchase.get();
+
+  //Hacemos un ForeEach para agregarlos a la lista
+  queryPurchase.docs.forEach((usersfb) {
+    users.add(usersfb.data());
+  });
+  //Accion para esperar a que se llene la lista
+  Future.delayed(const Duration(seconds: 2));
+
+  return users;
+}
--- lib/views/user/user.dart
+++ lib/views/user/user.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase/components/headerList.dart';
+import 'package:flutter_firebase/views/services/user-service/user_service.dart';
 
 class UserScreen extends StatelessWidget {
-  const UserScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromRGBO(25, 23, 32, 1),
       extendBody: true,
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24),
+        padding: const EdgeInsets.symmetric(horizontal: 12),
         child: Column(
           children: [
             HeaderList(
@@ -18,8 +17,82 @@ class UserScreen extends StatelessWidget {
               onBack: () {
                 Navigator.pop(context);
               },
-            )
+            ),
+            FutureBuilder(
+              future: getUsers(),
+              builder: ((context, snapshot) {
+                if (snapshot.hasData) {
+                  return Expanded(
+                    child: ListView.builder(
+                      itemCount: snapshot.data?.length,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          margin: EdgeInsets.only(bottom: 12),
+                          child: ListTile(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8.0),
+                              side:
+                                  BorderSide(color: Color(0xFF3D3C46), width: 2),
+                            ),
+                            leading: Icon(Icons.account_circle_sharp,
+                                color: Colors.white, size: 40),
+                            trailing: Icon(
+                              Icons.edit,
+                              color: Color.fromARGB(150, 230, 230, 230),
+                            ),
+                            title: Padding(
+                              padding: const EdgeInsets.only(top: 12),
+                              child: Text(
+                                snapshot.data?[index]['name'],
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                            subtitle: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Email: " + snapshot.data?[index]['email'],
+                                  style: TextStyle(
+                                      color: Color.fromARGB(150, 230, 230, 230)),
+                                ),
+                                Text(
+                                  "Gender: " + snapshot.data?[index]['gender'],
+                                  style: TextStyle(
+                                      color: Color.fromARGB(150, 230, 230, 230)),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(bottom: 12),
+                                  child: Text(
+                                    "Age: " + snapshot.data?[index]['age'],
+                                    style: TextStyle(
+                                        color: Color.fromARGB(150, 230, 230, 230)),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  );
+                } else {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+              }),
+            ),
           ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.white,
+        onPressed: () {
+          Navigator.pushNamed(context, '/addPurchase');
+        },
+        child: const Icon(
+          Icons.add,
+          color: Colors.black,
         ),
       ),
     );
